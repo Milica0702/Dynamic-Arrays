e3872909c516ad05e441432debedd2813760b5d8
Milica0702 <m.bogosavljevic0702@gmail.com>
2020-03-16 18:43:44 +0100

    Dodavanje add Funkcija

--- src/implementacijadinamickihnizova/ArrayList.java
+++ src/implementacijadinamickihnizova/ArrayList.java
@@ -7,21 +7,46 @@ public class ArrayList {
     //removeElement
     //printList
     //elementAtIndexOf 
+    int k = 3;
+    int [] niz = new int [k];
+    int pokazivac = 0;
     
-    int [] niz = new int [3];
-    
-    
-    /*nije static moramo da imamo niz u koji cemo da stavimo
+    //nije static moramo da imamo niz u koji cemo da stavimo
     public void addElement (int element){
+       if (pokazivac == niz.length-1){
+           int [] temp = new int [niz.length]; //k
+           for (int i  = 0; i < niz.length ; i++){
+               temp [i] =niz[i];
+              
+           }
+           k=2*k;
+           this.niz = new int [k];
+           for (int i  = 0; i < temp.length ; i++){
+               niz[i]=temp [i];
+               
+              
+           }
+       } 
+       this.niz[pokazivac]= element ;
+       pokazivac++;
        
         
     }
-    */
+    
+   
     
     public void printList (){
        
-        for (int i = 0 ; i< niz.length ; i++){
-            System.out.println(niz[i]);
+        if (pokazivac == 0 ){
+            System.out.println("Niz je prazan");
+        }else {
+            for (int i = 0 ; i< pokazivac ; i++){
+                System.out.println(niz[i]);
+            }
         }
+    
     }
+    
+    
+    
 }
--- src/implementacijadinamickihnizova/ImplementacijaDinamickihNizova.java
+++ src/implementacijadinamickihnizova/ImplementacijaDinamickihNizova.java
@@ -6,6 +6,16 @@ public class ImplementacijaDinamickihNizova {
 
     public static void main(String[] args) {
        ArrayList al = new ArrayList();
+       
+       al.addElement(1);
+       al.addElement(2);
+       al.addElement(3);
+       al.addElement(4);
+       al.addElement(5);
+       al.addElement(6);
+       al.addElement(7);
+     
+      
        al.printList();
     }
     
